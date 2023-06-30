6ab40b1c6b26e5648a77f3d0212f24492ac1b7e8
sergiorjsh041 <Sergio.rojas041@gmail.com>
2023-06-30 10:22:23 -0400

    feat: logica de maquina enigma

--- enigma_machine.py
+++ enigma_machine.py
@@ -2,40 +2,46 @@ from utils import *
 from rotor import Rotor
 
 rotors, offsets, plugboard, reflector = read_configuration()
-print(rotors, offsets, plugboard, reflector)
+#print(rotors, offsets, plugboard, reflector)
 
-message = "alshgkjfgsdfkhsdf".upper()
+message = "msxy".upper()
 
 rot1 = Rotor(rotors[0], offsets[0])
 rot2 = Rotor(rotors[1], offsets[1])
 rot3 = Rotor(rotors[2], offsets[2])
 rot4 = Rotor(rotors[3], offsets[3])
-
+refl = Rotor(reflector, 0)
 
 
 
 encripted= ""
 for letter in message:
-    let = letter
-    let = plugboard[let]
-    let = rot1.input(let)
-    let = rot2.input(let)
-    let = rot3.input(let)
-    let = rot4.input(let)
+    if ((ord(letter)<ord('A')) or (ord(letter)>ord('Z'))):
+        encripted+=letter
+    else:  
+        let = letter
+    
+        let = plugboard[let]
+    
+        let = rot1.input(let)
+        let = rot2.input(let)
+        let = rot3.input(let)
+        let = rot4.input(let)
+    
+        let = refl.reflect(let)
 
-    let = reflector.reflect(let)
+        let = rot4.output(let)
+        let = rot3.output(let)
+        let = rot2.output(let)
+        let = rot1.output(let)
 
-    let = rot4.output(let)
-    let = rot3.output(let)
-    let = rot2.output(let)
-    let = rot1.output(let)
+        let = plugboard[let]
+    
+        encripted+= let
 
-    let = plugboard[let]
-    encripted+= let
-
-    next = rot1.rotate(1)
-    next = rot2.rotate(next)
-    next = rot3.rotate(next)
-    next = rot4.rotate(next)
+        next = rot1.rotate(1)
+        next = rot2.rotate(next)
+        next = rot3.rotate(next)
+        next = rot4.rotate(next)
 
 print(encripted)
--- rotor.py
+++ rotor.py
@@ -1,37 +1,84 @@
 from utils import *
-
 def rotor_connections(type:str):
     # Dado un string de tipo de rotor/refelector, 
     # entrega el diccionario de permutaciones y lista de muescas correspondiente
-    raise NotImplementedError
+    if type=='I':
+        con=I
+        muescas=[24]
+    elif type=='II':
+        con=II
+        muescas=[12]
+    elif type=='III':
+        con=III
+        muescas=[3]
+    elif type=='IV':
+        con=IV
+        muescas=[17]
+    elif type=='V':
+        con=V
+        muescas=[7]
+    elif type=='VI':
+        con=VI
+        muescas=[7,20]
+    elif type=='VII':
+        con=VII
+        muescas=[7,20]
+    elif type=='VIII':
+        con=VIII
+        muescas=[0,21]
+    elif type=='Beta':
+        con=BETA
+        muescas=[]
+    elif type=='UKW-b':
+        con=UKW_B
+        muescas=[]
+    elif type=='UKW-c':
+        con=UKW_C
+        muescas=[]
+    return (con,muescas)
 
 
 class Rotor:
     connections = {}
     position = 0
     muescas = []
-
+    
+    def __init__(self, rotor, offset):
+        con,muescas = rotor_connections(rotor)
+        self.connections = con
+        self.muescas = muescas
+        self.position = offset
+        
     def rotate(self, places:int):
         next = 0
         if self.position in self.muescas:
             next = 1
         self.position += places
+        
         return next
     
-
     def input(self, letter:str):
-        raise NotImplementedError
+        input_simulado = chr((ord(letter) + self.position - 65) % 26 + 65)
+        output_simulado = self.connections[input_simulado]
+        output_real = chr((ord(output_simulado) - self.position - 65) % 26 + 65)
+
+        return output_real
 
     def output(self, letter:str):
-        raise NotImplementedError
+        con=self.connections
+        output_simulado = chr((ord(letter) + self.position - 65) % 26 + 65)
+        input_simulado=list(con.keys())[(list(con.values()).index(output_simulado))]
+        input_real=chr((ord(input_simulado) - self.position - 65) % 26 + 65)
+
+        return input_real
     
     def reflect(self, letter:str):
         return self.input(letter)
 
 
-    def __init__(self, type:str, init_pos:int, _slots:list[int]):
-        self.connections, self.muescas= rotor_connections(type)
-        self.position = init_pos
+    # def __init__(self, type:str, init_pos:int, _slots:list[int]):
+    #     self.connections, self.muescas= rotor_connections(type)
+    #     self.position = init_pos
 
-        return self
+    #     return self
 
